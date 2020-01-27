a4c73f37cb60b02c6dc7f7d781fb06850a5f8a0a
ibrahim45 <kibrahimsha4@gmail.com>
2020-01-27 12:46:18 +0530

    updated pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dictlookup',
-      version='0.1',
+      version='0.2',
       description='Search a word in dictionary',
       url='http://github.com/ibrahim45/dict-lookup',
       author='Ibrahim sha',
