18f6bb7dc584dcae4fc348cf4c00c4be4d6a6f73
ibrahim45 <kibrahimsha4@gmail.com>
2020-01-28 23:57:26 +0530

    updated setup.py in dictlookup

--- dictlookup/__init__.py
+++ dictlookup/__init__.py
@@ -3,7 +3,6 @@ import requests
 import pprint
 
 API_TOKEN = os.environ.get('API_TOKEN')
-print(API_TOKEN)
 OWL_URI = 'https://owlbot.info/api/v4/dictionary/'
 token = 'Token  ' + API_TOKEN
 pp = pprint.PrettyPrinter(indent=4)
